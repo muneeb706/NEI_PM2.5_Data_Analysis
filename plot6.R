c74c92ea1bea257e586ae5d4c84fe09d6430c9d8
muneeb706 <muneeb706@gmail.com>
2020-07-23 05:50:42 -0500

    comments updated.

--- plot6.R
+++ plot6.R
@@ -1,6 +1,6 @@
 # Question: 6
 # Compare emissions from motor vehicle sources in Baltimore City with emissions from motor vehicle sources in Los Angeles County, 
-# California fips == "06037". 
+# California (fips == "06037"). 
 # Which city has seen greater changes over time in motor vehicle emissions?
 
 ## This first line will likely take a few seconds. Be patient!
